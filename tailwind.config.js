426ba5770d3af4bdd74561d1174dcb6a8a49a9d7
ebundala <ebundala@gmail.com>
2023-02-21 14:38:42 +0300

    Initial comit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      "./screens/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./hooks/**/*.{js,ts,jsx,tsx}",
+      "./types/**/*.{js,ts,jsx,tsx}",
+    ],
+    // ...
+  };
