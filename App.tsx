dcaee41a0b7d11670e4bbc3acca183e8c99137e8
ebundala <ebundala@gmail.com>
2023-02-22 08:40:37 +0300

    chore(Config): Add config and deps

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './screens/HomeScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <SafeAreaProvider>
+    <StatusBar style='auto'/>
+    <NavigationContainer>             
     <Stack.Navigator>
       <Stack.Screen name="home" component={HomeScreen} />
     </Stack.Navigator>
   </NavigationContainer>
+  </SafeAreaProvider>
   );
 }
 
